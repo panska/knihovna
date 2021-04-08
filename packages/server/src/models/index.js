5676421b437d8b028c301d428a7cc1f1889644af
Kovačević Marko <markokovacevic@email.cz>
2021-04-08 17:27:00 +0200

    Add retry on error to database config

--- packages/server/src/models/index.js
+++ packages/server/src/models/index.js
@@ -8,6 +8,21 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+config.retry = {
+  max: Infinity,
+  match: [
+    /ConnectionError/,
+    /SequelizeConnectionError/,
+    /SequelizeConnectionRefusedError/,
+    /SequelizeHostNotFoundError/,
+    /SequelizeHostNotReachableError/,
+    /SequelizeInvalidConnectionError/,
+    /SequelizeConnectionTimedOutError/,
+    /SequelizeConnectionAcquireTimeoutError/,
+    /Connection terminated unexpectedly/,
+  ],
+};
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
